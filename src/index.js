e86856d5aec58a40375f3ead5f7caa21895b4250
Josua <josua.reh@icloud.com>
2023-02-01 11:34:21 +0100

    rest

--- src/index.js
+++ src/index.js
@@ -50,8 +50,61 @@ class MyGame extends Phaser.Scene {
             frameRate: 10,
             repeat: -1,
         });
+        //stars
+        const stars = this.physics.add.group({
+            key: "star",
+            repeat: 11,
+            setXY: { x: 12, y: 0, steX: 70},
+        });p 
+        stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.overlap(this.player, stars, collect, null, this);
+       //bombs
+       const bombs = this.physics.add.group();
+       this.physics.add.collider(bombs, platforms);
+       this.physics.add.collider(this.player, bombs, bombTouched, null, this); 
 
-         
+       function bombTouched(player, bomb) {
+        this.physics.pause();
+        this.player.setTint(0xff000);
+        this.player.anims.play("turn");
+       }
+
+       //score text
+       const scoreText = this.add.text(15, 15, "score: 0", {
+        fontSize: "32px",
+        fill: "#000",
+       })
+       let score = 0
+
+
+       //stars collision 
+        function collect(player, star) {
+            star.disableBody(true, true);
+            score += 1;
+            scoreText.setText("Score: "+ score);
+
+            if(stars.countActive(true) === 0) {
+              stars.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+              });
+            } 
+            var x = 
+              player.x < 400
+                ? Phaser.Math.Between(400, 800)
+                : Phaser.Math.Between(0, 400);
+
+            const bomb = bombs.create(x, 16, "bomb")    
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)    
+
+
+
+
+      }
     }
     
     update() {
@@ -71,6 +124,7 @@ class MyGame extends Phaser.Scene {
         if (cursors.up.isDown && this.player.touching.down){
             this.player.setVelocityY(-420 )
         }
+
     }
 }
 
